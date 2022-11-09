6eabcd56eb052eb733c621068b86c4cf3663652b
ilariaorsi <92711758+ilariaorsi@users.noreply.github.com>
2022-11-09 21:56:16 +0100

    Update ActionInitialization.cc

--- Progetto Detector/src/ActionInitialization.cc
+++ Progetto Detector/src/ActionInitialization.cc
@@ -1,6 +1,5 @@
 #include "ActionInitialization.hh"
 #include "PrimaryGeneratorAction.hh"
-//! Optional user classes
 #include "RunAction.hh"
 #include "StackingAction.hh"
 #include "SteppingAction.hh"
@@ -21,6 +20,5 @@ void ActionInitialization::Build() const
 
 void ActionInitialization::BuildForMaster() const
 {
-  // By default, don't do anything. This applies only in MT mode:
   SetUserAction(new RunAction());
 }

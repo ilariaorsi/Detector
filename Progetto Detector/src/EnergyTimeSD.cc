561b7d92d6d2b05a9d6bb3d18bf426f8458d51f1
ilariaorsi <92711758+ilariaorsi@users.noreply.github.com>
2022-11-09 22:00:47 +0100

    Update EnergyTimeSD.cc

--- Progetto Detector/src/EnergyTimeSD.cc
+++ Progetto Detector/src/EnergyTimeSD.cc
@@ -1,5 +1,4 @@
 #include "EnergyTimeSD.hh"
-
 #include <G4SDManager.hh>
 #include <G4SystemOfUnits.hh>
 
@@ -11,12 +10,7 @@ EnergyTimeSD::EnergyTimeSD(G4String name) :
 
 G4bool EnergyTimeSD::ProcessHits(G4Step* aStep, G4TouchableHistory* /*ROhist*/)
 {
-    // Task 4d.1: Get total energy deposit, global time and position from the step
-
     EnergyTimeHit* hit = new EnergyTimeHit();
-    // Task 4d.1: Fill in the hit properties
-    
-    // Task 4d.1: Add the hit to the collection
     return true;
 }
 
